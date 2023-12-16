ac486d3a159c3e36229400c284eb37204eab7d69
Afdhol <afdholbasil@gmail.com>
2023-12-16 13:39:08 +0700

    3

---
+++ src/No15/KonversiWaktu.java
@@ -0,0 +1,26 @@
+package No15;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+public class KonversiWaktu {
+
+	public static void main(String[] args) throws ParseException {
+
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Input: ");
+
+		String input = scan.nextLine();
+		try {
+			SimpleDateFormat displayFormat = new SimpleDateFormat("HH:mm:ss");
+			SimpleDateFormat parseFormat = new SimpleDateFormat("hh:mm:ss a");
+			Date date = parseFormat.parse(input);
+			System.out.println(parseFormat.format(date) + " = " + displayFormat.format(date));
+
+		} catch (Exception e) {
+			System.out.println("input salah, contoh = 03:40:44 PM");
+		}
+	}
+}
--- src/No17/NinjaHattori.java
+++ src/No17/NinjaHattori.java
@@ -1,10 +1,36 @@
 package No17;
 
+import java.util.Scanner;
+
 public class NinjaHattori {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("CEK NINJA HATORI");
+		Scanner masukan = new Scanner(System.in);
+
+		System.out.print("Masukkan jalur ekspedisi : ");
+		String[] jalur = masukan.nextLine().toLowerCase().split(" ");
+
+		int tinggi = 0;
+		int gunung = 0;
+		int lembah = 0;
+		
+		for (int i = 0; i < jalur.length; i++) {
+			if (jalur[i].equals("n")) {
+				tinggi++;
+			} else if (jalur[i].equals("t")) {
+				tinggi--;
+			}
+			if ((jalur[i].equals("t")) && (tinggi == 0)) {
+				gunung++;
+			} else if ((jalur[i].equals("n")) && (tinggi == 0)) {
+				lembah++;
+			}
+		}
+		
+		System.out.println("Gunung yang sudah dilewati  = " + gunung);
+		System.out.println("Lembah yang sudah dilewati  = " + lembah);
+		masukan.close();
 	}
 
 }
