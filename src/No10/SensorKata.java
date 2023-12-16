ec66b849beddf1d3af349af55a2b68de5ef35328
Afdhol <afdholbasil@gmail.com>
2023-12-17 01:22:18 +0700

    ss

---
+++ src/No10/SensorKata.java
@@ -0,0 +1,21 @@
+package No10;
+
+import java.util.Scanner;
+
+public class SensorKata {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scan = new Scanner(System.in);
+		System.out.println("input: ");
+		
+		String str = scan.nextLine();
+		String repl = str.replaceAll("\\B\\w\\B", "*");
+		//=> s***********w
+		
+		System.out.println(repl);
+		
+	}
+
+}
---
+++ src/No13/Sudut.java
@@ -0,0 +1,33 @@
+package No13;
+
+import java.util.Scanner;
+
+public class Sudut {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("input: ");
+		
+		String[] clock = scanner.nextLine().split(":");
+		
+        int hour = Integer.parseInt(clock[0]);
+        int minute = Integer.parseInt(clock[1]);
+        
+		
+    System.out.println(cal_angle(hour,minute));
+	}
+
+	public static int cal_angle(int hour, int min) {
+		int h_angle = (int) ((hour * 60 + min) * 0.5);
+		int angle = (min * 6) - h_angle;
+		// now calculate minimum angle...
+
+		if (angle < 0)
+			angle = -angle;
+		return Math.min(360 - angle, angle);
+
+	}
+
+}
